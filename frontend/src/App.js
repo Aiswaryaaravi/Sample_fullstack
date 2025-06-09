35caa6d7ef061cc9f362aae9f15e60c02e56f801
Aiswaryaaravi <r.aiswarya2004@gmail.com>
2025-06-09 14:19:41 +0530

    Added files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <h1>Welcome!!</h1>
+    </div>
+  );
+}
+
+export default App; 
